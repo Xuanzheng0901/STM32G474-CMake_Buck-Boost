979e2a183fdd34d7da553bbefbc03bb28c65269f
Xuanzheng0901 <szx1691196289@gmail.com>
2026-07-26 21:42:32 +0800

    feat(adc): 修改追加

--- User/components/pid_ctrl/PID.h
+++ User/components/pid_ctrl/PID.h
@@ -1,10 +1,20 @@
 #ifndef G474_1_PID_H
 #define G474_1_PID_H
 #include <stdint.h>
+#include "main.h"
+
+typedef struct {
+    uint32_t adc12_buf[ADC_BUFFER_LENGTH];
+    uint16_t adc3_buf[ADC_BUFFER_LENGTH];
+} adc_buf_t;
+
+/** @brief DMA半缓冲区指针对，用于队列传递 */
+typedef struct {
+    uint32_t *adc12_half;  /*!< 指向 adc12_buf 当前半区的指针 */
+    uint16_t *adc3_half;   /*!< 指向 adc3_buf 对应半区的指针 */
+} adc_dma_block_t;
 
 void pid_ctrl_init(void);
-
-float get_voltage_value(uint8_t index);
 
 void pid_set_voltage(uint32_t mv);
 
