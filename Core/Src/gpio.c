036ab4a80a8ede6781e786a28f91b35cd91c5b63
Xuanzheng0901 <szx1691196289@gmail.com>
2026-07-30 21:52:26 +0800

    feat(ctrl): 为PD9增加下拉输入

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -141,7 +141,7 @@ void MX_GPIO_Init(void)
   /*Configure GPIO pin : PD9 */
   GPIO_InitStruct.Pin = GPIO_PIN_9;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PD0 SPI3_RESET_Pin */
